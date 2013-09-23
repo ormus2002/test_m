004cec32f2e91a0f084e93bb881eceab6637e390
ormus <ormus2002@gmail.com>
2013-09-23 13:31:09 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestM::Application.config.secret_token = 'f171ca16ae0c3d9ab2fc4c71b03f9164e773dbf37208cba191dca634e9a94fa7dec67e984e94ba0f90e2eb6906ae551d826347421eec2af269409b2a0e1459aa'
