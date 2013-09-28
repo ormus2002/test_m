20820f2d2e53ff8a49a518ad3d52c5c0c50850a7
ormus <ormus2002@gmail.com>
2013-09-28 19:48:16 +0300

    Add datepicker and edit  user's 'visible_for_bux' by manager

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    $('#dr .input-append.date').datepicker({
+        format: "dd.mm.yyyy",
+        language: "ru",
+        autoclose: true
+    });
+})
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 # -*- encoding : utf-8 -*-
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :check_if_admin,  only: [:edit, :update, :destroy]
+  before_filter :check_if_admin,  only: :destroy
   before_filter :check_roles_groups, only: :update
+  before_filter :check_if_admin_or_manager, only: [:edit, :update]
 
   def index
     if manager?(current_user)
@@ -33,11 +34,29 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if admin?(current_user)
+      render 'users/edit'
+    else
+      render 'users/edit_by_manager'
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
+    if manager?(current_user)
+      update_by_manager
+    else
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Профиль успешно обновлен"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+  end
+
+  def update_by_manager
+    if @user.update_attributes(visible_for_bux: params[:user][:visible_for_bux])
       flash[:success] = "Профиль успешно обновлен"
       redirect_to @user
     else
@@ -53,17 +72,19 @@ class UsersController < ApplicationController
 
   private
     def check_roles_groups
-      role_t = Role.find(params[:user][:role_id])
-      group_t = Group.find(params[:user][:group_id])
-      if (group_t.builtin == 1) && (role_t.builtin == 3 || role_t.builtin == 5)
-        flash[:error] = "#{role_t.name} не может иметь доступ в группу #{group_t.name}."
-        @user = User.find(params[:id])
-        render 'edit'
-      end
-      if (group_t.builtin == 2) && (role_t.builtin == 2)
-        flash[:error] = "#{role_t.name} не может иметь доступ в группу #{group_t.name}."
-        @user = User.find(params[:id])
-        render 'edit'
+      if !manager?(current_user)
+        role_t = Role.find(params[:user][:role_id])
+        group_t = Group.find(params[:user][:group_id])
+        if (group_t.builtin == 1) && (role_t.builtin == 3 || role_t.builtin == 5)
+          flash[:error] = "#{role_t.name} не может иметь доступ в группу #{group_t.name}."
+          @user = User.find(params[:id])
+          render 'edit'
+        end
+        if (group_t.builtin == 2) && (role_t.builtin == 2)
+          flash[:error] = "#{role_t.name} не может иметь доступ в группу #{group_t.name}."
+          @user = User.find(params[:id])
+          render 'edit'
+        end
       end
     end
 end
