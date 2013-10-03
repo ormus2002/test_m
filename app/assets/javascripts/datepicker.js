6b434f9b0f37bd89e4ac6168e3ed9d05e55e39f5
ormus <ormus2002@gmail.com>
2013-10-03 16:37:13 +0300

    Add script app.js for drop-down menu

--- app/assets/javascripts/datepicker.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-    $('#dr .input-append.date').datepicker({
-        format: "dd.mm.yyyy",
-        language: "ru",
-        autoclose: true
-    });
-})
---
+++ app/assets/javascripts/datepicker.js.coffee
@@ -0,0 +1,5 @@
+jQuery ->
+  $('#dr .input-append.date').datepicker
+    format: "dd.mm.yyyy",
+    language: "ru",
+    autoclose: true
