9d1fd18cbe62a37d7585e2aa5ab2f8136f6cee38
ormus <ormus2002@gmail.com>
2013-10-05 19:06:24 +0300

    Add unicorn to Gemfile Edit user.dr fields, if they missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ TestM::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options ={ :host => 'ormus2002-test_m.heroku.com' }
+  config.action_mailer.default_url_options ={ :host => 'test-m.ormus.lclients.ru' }
 
 end
